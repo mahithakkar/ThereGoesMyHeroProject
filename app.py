a0c6e28f600fb557a68d4ba8cfbf3901894c15c3
Mahi Thakkar <mahigthakkar2005@gmail.com>
2025-08-28 14:58:57 -0400

    minimal end to end chatbot finished

--- app.py
+++ app.py
@@ -16,9 +16,47 @@ client = GroqClient() #making a groq client object- from groq client class and e
 if not client.is_available():
     st.stop()
 
+
 #simple message exchange 
-user_prompt = st.text_input("Type your message: ")
-if user_prompt:
-    with st.spinner("Mahi is thinking!"):
-        reply = client.simple_message(user_prompt)
-    st.markdown(f"Mahi says: {reply}")
+#user_prompt = st.text_input("Type your message: ")
+#if user_prompt:
+#    with st.spinner("Mahi is thinking!"):
+#        reply = client.simple_message(user_prompt)
+#   st.markdown(f"Mahi says: {reply}")
+#checked and it works so proceeding to make a ui 
+
+
+#next logical state is focusing on UI and chat message
+#first step is to keep a simple chat history is session state 
+
+#session state is a way to share vars for reruns from api reference streamlit docs 
+#initally this would be empty 
+if "messages" not in st.session_state:
+    st.session_state.messages = [] #made a list of dictionary with each item as role: user/assistant, content: str 
+    
+#rendering existing messages
+#so for each message "m" in the list: 
+#key and value pair for each item
+for m in st.session_state.messages:
+    with st.chat_message(m["role"]):
+        st.markdown(m["content"])
+
+#input text on bottom 
+prompt = st.chat_input("Ask any TGMH question!")
+
+#on submit, append user then call groq then append assistant 
+if prompt:
+    st.session_state.messages.append({"role" : "user" , "content" : prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+        
+    with st.chat_message("assistant"):
+        with st.spinner("Mahi is thinking: "):
+            reply = client.simple_message(prompt)
+        st.markdown(reply)
+    
+    #adding it back to session state for history 
+    st.session_state.messages.append({"role" : "assistant" , "content" : reply})
+        
+    
+    
